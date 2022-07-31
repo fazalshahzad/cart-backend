182342f433c870b1e6add810e2d0a7a6007c4020
fazalshahzad <shancompany322@gmail.com>
2022-07-31 15:35:25 +0500

    c

--- controller/ProductManagementController.js
+++ controller/ProductManagementController.js
@@ -1,12 +1,29 @@
-const ProductData = (req, res) => {
+const ProductModel = require('../models/ProductManagementModels');
+
+
+const ProductData = async (req, res) => {
   
   
     try {
+        const {ProductName, ProductPrice} = req.body;
+
+
+        const DocToCreate = new ProductModel({
+            ProductName,
+            ProductPrice
+        })
+
+        const DocToSave = await DocToCreate.save();
+
         res.json({
             Message:'You Have reached the API end point',
-            Body:req.body
+            Body:`${ProductName}\n${ProductPrice}`
         })
     } catch (error) {
+
+        
+
+
         res.json({
             Message:error.Message,
             Body:null
