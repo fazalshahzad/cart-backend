93758611fafffc2d864fc21398b8c6027ef2f1e6
fazalshahzad <shancompany322@gmail.com>
2022-08-06 18:08:58 +0500

    commit

--- controller/ProductManagementController.js
+++ controller/ProductManagementController.js
@@ -2,10 +2,10 @@ const ProductModel = require('../models/ProductManagementModels');
 
 
 const ProductData = async (req, res) => {
-  
-  
+
+
     try {
-        const {ProductName, ProductPrice} = req.body;
+        const { ProductName, ProductPrice } = req.body;
 
 
         const DocToCreate = new ProductModel({
@@ -16,40 +16,67 @@ const ProductData = async (req, res) => {
         const DocToSave = await DocToCreate.save();
 
         res.json({
-            Message:'You Have reached the API end point',
-            Body:`${ProductName}\n${ProductPrice}`
+            Message: 'You Have reached the API end point',
+            Body: `${ProductName}\n${ProductPrice}`
         })
     } catch (error) {
 
-        
+
 
 
         res.json({
-            Message:error.Message,
-            Result:null,
-            Data:false
+            Message: error.Message,
+            Result: null,
+            Data: false
         })
-        
+
     }
 
 }
-const GetProductData = async(req, res) => {
+const GetProductData = async (req, res) => {
     try {
-        const DocToGet = await ProductModel.find();
+        const DocToGet = await ProductModel.find({}, { ProductPrice: 0 });
         res.json({
             Message: `Document found`,
-            Data:true,
-            Result:DocToGet
+            Data: true,
+            Result: DocToGet
         })
     } catch (error) {
-        
+        res.json({
+            Message: error.Message,
+            Result: null,
+            Data: false})
+
     }
-    
+
 }
+
+
+const UpdateMyProductData = async(req, res) => {
+    try {
+
+        const DocToUpdate = await ProductModel.updateMany(
+            {Status:0},
+            {Status:1}
+        )
+        res.json({
+            Message: `Document Updated`,
+            Data: true,
+            Result: DocToUpdate
+        })
+    } catch (error) {
+        res.json({
+            Message: error.Message,
+            Result: null,
+            Data: false})
+    }
+}
+
 
 module.exports = {
     ProductData,
-    GetProductData
+    GetProductData,
+    UpdateMyProductData
 
 }
 
--- routes/ProductManagementRoute.js
+++ routes/ProductManagementRoute.js
@@ -2,12 +2,14 @@ const express = require('express');
 const Router = express.Router();
 //calling my controllers
 const {ProductData,
-    GetProductData
+    GetProductData,
+    UpdateMyProductData
 } = require('../controller/ProductManagementController')
 
 
 Router.post('/ProductData',ProductData);
 Router.get('/GetProductData',GetProductData);
+Router.post('/UpdateMyProductData',UpdateMyProductData);
 
 
 module.exports = Router;
