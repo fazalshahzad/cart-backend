566b0a955610b123f6a499c14c35f925d1889614
fazalshahzad <shancompany322@gmail.com>
2022-08-06 18:39:13 +0500

    c

--- routes/ProductManagementRoute.js
+++ routes/ProductManagementRoute.js
@@ -3,13 +3,15 @@ const Router = express.Router();
 //calling my controllers
 const {ProductData,
     GetProductData,
-    UpdateMyProductData
+    UpdateMyProductData,
+    DeleteMyProductData
 } = require('../controller/ProductManagementController')
 
 
 Router.post('/ProductData',ProductData);
 Router.get('/GetProductData',GetProductData);
 Router.post('/UpdateMyProductData',UpdateMyProductData);
+Router.post('/DeleteMyProductData',DeleteMyProductData);
 
 
 module.exports = Router;
